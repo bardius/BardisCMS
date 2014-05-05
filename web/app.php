ac0917c22242c731b3b21d709cda6c6053a177b5
bardius <george.bardis@maynardmalone.com>
2014-05-05 17:50:20 +0100

    preparing the distribution for hosting at AWS bitnami AMI's

--- web/app.php
+++ web/app.php
@@ -9,8 +9,9 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Change 'sf2' by the prefix you want in order to prevent key conflict with another application
 /*
 */
-//$loader = new ApcClassLoader('tgs', $loader);
-//$loader->register(true);
+//$apcLoader = new ApcClassLoader('bardiscms', $loader);
+//$loader->unregister();
+//$apcLoader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
 require_once __DIR__.'/../app/AppCache.php';
@@ -18,8 +19,10 @@ require_once __DIR__.'/../app/AppCache.php';
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 $kernel = new AppCache($kernel);
-$request = Request::createFromGlobals();
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
