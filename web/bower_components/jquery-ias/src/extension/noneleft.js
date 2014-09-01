11f1b021a0008d47f3bcd309d34dcb36937fadfd
bardius <george.bardis@maynardmalone.com>
2014-09-01 23:03:25 +0100

    merging and adding automated build configuration

---
+++ web/bower_components/jquery-ias/src/extension/noneleft.js
@@ -0,0 +1,47 @@
+/**
+ * IAS None Left Extension
+ * An IAS extension to show a message when there are no more pages te load
+ * http://infiniteajaxscroll.com
+ *
+ * This file is part of the Infinite AJAX Scroll package
+ *
+ * Copyright 2014 Webcreate (Jeroen Fiege)
+ */
+
+var IASNoneLeftExtension = function(options) {
+  options = $.extend({}, this.defaults, options);
+
+  this.ias = null;
+  this.uid = (new Date()).getTime();
+  this.html = (options.html).replace('{text}', options.text);
+
+  /**
+   * Shows none left message
+   */
+  this.showNoneLeft = function() {
+    var $element = $(this.html).attr('id', 'ias_noneleft_' + this.uid),
+        $lastItem = this.ias.getLastItem();
+
+    $lastItem.after($element);
+    $element.fadeIn();
+  };
+
+  return this;
+};
+
+/**
+ * @public
+ */
+IASNoneLeftExtension.prototype.bind = function(ias) {
+  this.ias = ias;
+
+  ias.on('noneLeft', $.proxy(this.showNoneLeft, this));
+};
+
+/**
+ * @public
+ */
+IASNoneLeftExtension.prototype.defaults = {
+  text: 'You reached the end.',
+  html: '<div class="ias-noneleft" style="text-align: center;">{text}</div>'
+};
---
+++ web/bower_components/jquery-ias/src/extension/trigger.js
@@ -0,0 +1,144 @@
+/**
+ * IAS Trigger Extension
+ * An IAS extension to show a trigger link to load the next page
+ * http://infiniteajaxscroll.com
+ *
+ * This file is part of the Infinite AJAX Scroll package
+ *
+ * Copyright 2014 Webcreate (Jeroen Fiege)
+ */
+
+var IASTriggerExtension = function(options) {
+  options = $.extend({}, this.defaults, options);
+
+  this.ias = null;
+  this.html = (options.html).replace('{text}', options.text);
+  this.htmlPrev = (options.htmlPrev).replace('{text}', options.textPrev);
+  this.enabled = true;
+  this.count = 0;
+  this.offset = options.offset;
+  this.$triggerNext = null;
+  this.$triggerPrev = null;
+
+  /**
+   * Shows trigger for next page
+   */
+  this.showTriggerNext = function() {
+    if (!this.enabled) {
+      return true;
+    }
+
+    if (false === this.offset || ++this.count < this.offset) {
+      return true;
+    }
+
+    var $trigger = this.$triggerNext || (this.$triggerNext = this.createTrigger(this.next, this.html));
+    var $lastItem = this.ias.getLastItem();
+
+    $lastItem.after($trigger);
+    $trigger.fadeIn();
+
+    return false;
+  };
+
+  /**
+   * Shows trigger for previous page
+   */
+  this.showTriggerPrev = function() {
+    if (!this.enabled) {
+      return true;
+    }
+
+    var $trigger = this.$triggerPrev || (this.$triggerPrev = this.createTrigger(this.prev, this.htmlPrev));
+    var $firstItem = this.ias.getFirstItem();
+
+    $firstItem.before($trigger);
+    $trigger.fadeIn();
+
+    return false;
+  };
+
+  /**
+   * @param clickCallback
+   * @returns {*|jQuery}
+   * @param {string} html
+   */
+  this.createTrigger = function(clickCallback, html) {
+    var uid = (new Date()).getTime(),
+        $trigger;
+
+    html = html || this.html;
+    $trigger = $(html).attr('id', 'ias_trigger_' + uid);
+
+    $trigger.hide();
+    $trigger.on('click', $.proxy(clickCallback, this));
+
+    return $trigger;
+  };
+
+  return this;
+};
+
+/**
+ * @public
+ * @param {object} ias
+ */
+IASTriggerExtension.prototype.bind = function(ias) {
+  var self = this;
+
+  this.ias = ias;
+
+  try {
+    ias.on('prev', $.proxy(this.showTriggerPrev, this), this.priority);
+  } catch (exception) {}
+
+  ias.on('next', $.proxy(this.showTriggerNext, this), this.priority);
+  ias.on('rendered', function () { self.enabled = true; }, this.priority);
+};
+
+/**
+ * @public
+ */
+IASTriggerExtension.prototype.next = function() {
+  this.enabled = false;
+  this.ias.unbind();
+
+  if (this.$triggerNext) {
+    this.$triggerNext.remove();
+    this.$triggerNext = null;
+  }
+
+  this.ias.next();
+};
+
+/**
+ * @public
+ */
+IASTriggerExtension.prototype.prev = function() {
+  this.enabled = false;
+  this.ias.unbind();
+
+  if (this.$triggerPrev) {
+    this.$triggerPrev.remove();
+    this.$triggerPrev = null;
+  }
+
+  this.ias.prev();
+};
+
+/**
+ * @public
+ */
+IASTriggerExtension.prototype.defaults = {
+  text: 'Load more items',
+  html: '<div class="ias-trigger ias-trigger-next" style="text-align: center; cursor: pointer;"><a>{text}</a></div>',
+  textPrev: 'Load previous items',
+  htmlPrev: '<div class="ias-trigger ias-trigger-prev" style="text-align: center; cursor: pointer;"><a>{text}</a></div>',
+  offset: 0
+};
+
+/**
+ * @public
+ * @type {number}
+ */
+IASTriggerExtension.prototype.priority = 1000;
