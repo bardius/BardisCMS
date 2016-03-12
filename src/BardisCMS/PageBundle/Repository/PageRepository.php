d88e21e558c854dbee9d6bd930bb6f909bc4aebe
bardius <george@bardis.info>
2016-03-12 23:37:41 +0000

    removing the system pages from the sitemap.xml

--- src/BardisCMS/PageBundle/Repository/PageRepository.php
+++ src/BardisCMS/PageBundle/Repository/PageRepository.php
@@ -15,13 +15,13 @@ use Doctrine\ORM\EntityRepository;
 class PageRepository extends EntityRepository {
 
     // Function to retrieve the pages of a category with pagination
-    public function getCategoryItems($categoryIds, $currentPageId, $publishStates, $currentpage, $totalpageitems) {
+    public function getCategoryItems($categoryIds, $currentPageId, $publishStates, $currentPage, $totalPageItems) {
 
         $pageList = null;
 
         if (!empty($categoryIds)) {
 
-            // Initalize the query builder variables
+            // Initialize the query builder variables
             $qb = $this->_em->createQueryBuilder();
             $countqb = $this->_em->createQueryBuilder();
 
@@ -29,13 +29,19 @@ class PageRepository extends EntityRepository {
             $qb->select('DISTINCT p')
                     ->from('PageBundle:Page', 'p')
                     ->innerJoin('p.categories', 'c')
-                    ->where($qb->expr()->andX(
-                                    $qb->expr()->in('c.id', ':category'), $qb->expr()->in('p.publishState', ':publishState'), $qb->expr()->neq('p.id', ':currentPage'), $qb->expr()->neq('p.pagetype', ':categorypagePageType'), $qb->expr()->neq('p.pagetype', ':homepagePageType')
-                    ))
+                    ->where(
+                        $qb->expr()->andX(
+                            $qb->expr()->in('c.id', ':category'),
+                            $qb->expr()->in('p.publishState', ':publishState'),
+                            $qb->expr()->neq('p.id', ':currentPage'),
+                            $qb->expr()->neq('p.pagetype', ':categoryPagePageType'),
+                            $qb->expr()->neq('p.pagetype', ':homepagePageType')
+                        )
+                    )
                     ->orderBy('p.date', 'DESC')
                     ->setParameter('category', $categoryIds)
                     ->setParameter('publishState', $publishStates)
-                    ->setParameter('categorypagePageType', 'category_page')
+                    ->setParameter('categoryPagePageType', 'category_page')
                     ->setParameter('currentPage', $currentPageId)
                     ->setParameter('homepagePageType', 'homepage')
             ;
@@ -44,13 +50,19 @@ class PageRepository extends EntityRepository {
             $countqb->select('COUNT(DISTINCT p.id)')
                     ->from('PageBundle:Page', 'p')
                     ->innerJoin('p.categories', 'c')
-                    ->where($countqb->expr()->andX(
-                                    $countqb->expr()->in('c.id', ':category'), $countqb->expr()->in('p.publishState', ':publishState'), $countqb->expr()->neq('p.id', ':currentPage'), $countqb->expr()->neq('p.pagetype', ':categorypagePageType'), $countqb->expr()->neq('p.pagetype', ':homepagePageType')
-                    ))
+                    ->where(
+                        $countqb->expr()->andX(
+                            $countqb->expr()->in('c.id', ':category'),
+                            $countqb->expr()->in('p.publishState', ':publishState'),
+                            $countqb->expr()->neq('p.id', ':currentPage'),
+                            $countqb->expr()->neq('p.pagetype', ':categoryPagePageType'),
+                            $countqb->expr()->neq('p.pagetype', ':homepagePageType')
+                        )
+                    )
                     ->orderBy('p.date', 'DESC')
                     ->setParameter('category', $categoryIds)
                     ->setParameter('publishState', $publishStates)
-                    ->setParameter('categorypagePageType', 'category_page')
+                    ->setParameter('categoryPagePageType', 'category_page')
                     ->setParameter('currentPage', $currentPageId)
                     ->setParameter('homepagePageType', 'homepage')
             ;
@@ -58,19 +70,19 @@ class PageRepository extends EntityRepository {
             $totalResultsCount = intval($countqb->getQuery()->getSingleScalarResult());
 
             // Get the paginated results
-            $pageList = $this->getPaginatedResults($qb, $totalResultsCount, $currentpage, $totalpageitems);
+            $pageList = $this->getPaginatedResults($qb, $totalResultsCount, $currentPage, $totalPageItems);
         }
 
         return $pageList;
     }
 
     // Function to retrieve the pages of tag/category combination with pagination
-    public function getTaggedCategoryItems($categoryIds, $currentPageId, $publishStates, $currentpage, $totalpageitems, $tagIds) {
+    public function getTaggedCategoryItems($categoryIds, $currentPageId, $publishStates, $currentPage, $totalPageItems, $tagIds) {
 
         $pageList = null;
 
         if (!empty($categoryIds)) {
-            // Initalize the query builder variables
+            // Initialize the query builder variables
             $qb = $this->_em->createQueryBuilder();
             $countqb = $this->_em->createQueryBuilder();
 
@@ -79,14 +91,21 @@ class PageRepository extends EntityRepository {
                     ->from('PageBundle:Page', 'p')
                     ->innerJoin('p.categories', 'c')
                     ->innerJoin('p.tags', 't')
-                    ->where($qb->expr()->andX(
-                                    $qb->expr()->in('c.id', ':category'), $qb->expr()->in('t.id', ':tag'), $qb->expr()->in('p.publishState', ':publishState'), $qb->expr()->neq('p.id', ':currentPage'), $qb->expr()->neq('p.pagetype', ':categorypagePageType'), $qb->expr()->neq('p.pagetype', ':homepagePageType')
-                    ))
+                    ->where(
+                        $qb->expr()->andX(
+                            $qb->expr()->in('c.id', ':category'),
+                            $qb->expr()->in('t.id', ':tag'),
+                            $qb->expr()->in('p.publishState', ':publishState'),
+                            $qb->expr()->neq('p.id', ':currentPage'),
+                            $qb->expr()->neq('p.pagetype', ':categoryPagePageType'),
+                            $qb->expr()->neq('p.pagetype', ':homepagePageType')
+                        )
+                    )
                     ->orderBy('p.date', 'DESC')
                     ->setParameter('category', $categoryIds)
                     ->setParameter('tag', $tagIds)
                     ->setParameter('publishState', $publishStates)
-                    ->setParameter('categorypagePageType', 'category_page')
+                    ->setParameter('categoryPagePageType', 'category_page')
                     ->setParameter('currentPage', $currentPageId)
                     ->setParameter('homepagePageType', 'homepage')
             ;
@@ -96,14 +115,21 @@ class PageRepository extends EntityRepository {
                     ->from('PageBundle:Page', 'p')
                     ->innerJoin('p.categories', 'c')
                     ->innerJoin('p.tags', 't')
-                    ->where($countqb->expr()->andX(
-                                    $countqb->expr()->in('c.id', ':category'), $countqb->expr()->in('t.id', ':tag'), $countqb->expr()->in('p.publishState', ':publishState'), $countqb->expr()->neq('p.id', ':currentPage'), $countqb->expr()->neq('p.pagetype', ':homepagePageType'), $countqb->expr()->neq('p.pagetype', ':categorypagePageType')
-                    ))
+                    ->where(
+                        $countqb->expr()->andX(
+                            $countqb->expr()->in('c.id', ':category'),
+                            $countqb->expr()->in('t.id', ':tag'),
+                            $countqb->expr()->in('p.publishState', ':publishState'),
+                            $countqb->expr()->neq('p.id', ':currentPage'),
+                            $countqb->expr()->neq('p.pagetype', ':homepagePageType'),
+                            $countqb->expr()->neq('p.pagetype', ':categoryPagePageType')
+                        )
+                    )
                     ->orderBy('p.date', 'DESC')
                     ->setParameter('category', $categoryIds)
                     ->setParameter('tag', $tagIds)
                     ->setParameter('publishState', $publishStates)
-                    ->setParameter('categorypagePageType', 'category_page')
+                    ->setParameter('categoryPagePageType', 'category_page')
                     ->setParameter('currentPage', $currentPageId)
                     ->setParameter('homepagePageType', 'homepage')
             ;
@@ -111,20 +137,20 @@ class PageRepository extends EntityRepository {
             $totalResultsCount = intval($countqb->getQuery()->getSingleScalarResult());
 
             // Get the paginated results
-            $pageList = $this->getPaginatedResults($qb, $totalResultsCount, $currentpage, $totalpageitems);
+            $pageList = $this->getPaginatedResults($qb, $totalResultsCount, $currentPage, $totalPageItems);
         }
 
         return $pageList;
     }
 
     // Function to retrieve the pages of a tag with pagination
-    public function getTaggedItems($tagIds, $currentPageId, $publishStates, $currentpage, $totalpageitems) {
+    public function getTaggedItems($tagIds, $currentPageId, $publishStates, $currentPage, $totalPageItems) {
 
         $pageList = null;
 
         if (!empty($tagIds)) {
 
-            // Initalize the query builder variables
+            // Initialize the query builder variables
             $qb = $this->_em->createQueryBuilder();
             $countqb = $this->_em->createQueryBuilder();
 
@@ -132,28 +158,40 @@ class PageRepository extends EntityRepository {
             $qb->select('DISTINCT p')
                     ->from('PageBundle:Page', 'p')
                     ->innerJoin('p.tags', 't')
-                    ->where($qb->expr()->andX(
-                                    $qb->expr()->in('t.id', ':tag'), $qb->expr()->in('p.publishState', ':publishState'), $qb->expr()->neq('p.id', ':currentPage'), $qb->expr()->neq('p.pagetype', ':categorypagePageType'), $qb->expr()->neq('p.pagetype', ':homepagePageType')
-                    ))
+                    ->where(
+                        $qb->expr()->andX(
+                            $qb->expr()->in('t.id', ':tag'),
+                            $qb->expr()->in('p.publishState', ':publishState'),
+                            $qb->expr()->neq('p.id', ':currentPage'),
+                            $qb->expr()->neq('p.pagetype', ':categoryPagePageType'),
+                            $qb->expr()->neq('p.pagetype', ':homepagePageType')
+                        )
+                    )
                     ->orderBy('p.date', 'DESC')
                     ->setParameter('tag', $tagIds)
                     ->setParameter('publishState', $publishStates)
-                    ->setParameter('categorypagePageType', 'category_page')
+                    ->setParameter('categoryPagePageType', 'category_page')
                     ->setParameter('currentPage', $currentPageId)
                     ->setParameter('homepagePageType', 'homepage')
             ;
 
-            // The query to get the total page items count  
+            // The query to get the total page items count
             $countqb->select('COUNT(DISTINCT p.id)')
                     ->from('PageBundle:Page', 'p')
                     ->innerJoin('p.tags', 't')
-                    ->where($countqb->expr()->andX(
-                                    $countqb->expr()->in('t.id', ':tag'), $countqb->expr()->in('p.publishState', ':publishState'), $countqb->expr()->neq('p.id', ':currentPage'), $countqb->expr()->neq('p.pagetype', ':categorypagePageType'), $countqb->expr()->neq('p.pagetype', ':homepagePageType')
-                    ))
+                    ->where(
+                        $countqb->expr()->andX(
+                            $countqb->expr()->in('t.id', ':tag'),
+                            $countqb->expr()->in('p.publishState', ':publishState'),
+                            $countqb->expr()->neq('p.id', ':currentPage'),
+                            $countqb->expr()->neq('p.pagetype', ':categoryPagePageType'),
+                            $countqb->expr()->neq('p.pagetype', ':homepagePageType')
+                        )
+                    )
                     ->orderBy('p.date', 'DESC')
                     ->setParameter('tag', $tagIds)
                     ->setParameter('publishState', $publishStates)
-                    ->setParameter('categorypagePageType', 'category_page')
+                    ->setParameter('categoryPagePageType', 'category_page')
                     ->setParameter('currentPage', $currentPageId)
                     ->setParameter('homepagePageType', 'homepage')
             ;
@@ -161,7 +199,7 @@ class PageRepository extends EntityRepository {
             $totalResultsCount = intval($countqb->getQuery()->getSingleScalarResult());
 
             // Get the paginated results
-            $pageList = $this->getPaginatedResults($qb, $totalResultsCount, $currentpage, $totalpageitems);
+            $pageList = $this->getPaginatedResults($qb, $totalResultsCount, $currentPage, $totalPageItems);
         }
 
         return $pageList;
@@ -174,16 +212,20 @@ class PageRepository extends EntityRepository {
 
         if (!empty($categoryIds)) {
 
-            // Initalize the query builder variables
+            // Initialize the query builder variables
             $qb = $this->_em->createQueryBuilder();
 
             // The query to get the page items for the homepage page
             $qb->select('DISTINCT p')
                     ->from('PageBundle:Page', 'p')
                     ->innerJoin('p.categories', 'c')
-                    ->where($qb->expr()->andX(
-                                    $qb->expr()->in('c.id', ':category'), $qb->expr()->in('p.publishState', ':publishState'), $qb->expr()->neq('p.id', ':currentPage')
-                    ))
+                    ->where(
+                        $qb->expr()->andX(
+                            $qb->expr()->in('c.id', ':category'),
+                            $qb->expr()->in('p.publishState', ':publishState'),
+                            $qb->expr()->neq('p.id', ':currentPage')
+                        )
+                    )
                     ->orderBy('p.pageOrder', 'ASC')
                     ->setParameter('category', $categoryIds)
                     ->setParameter('publishState', $publishStates)
@@ -200,17 +242,21 @@ class PageRepository extends EntityRepository {
     // Function to retrieve a page list for sitemap
     public function getSitemapList($publishStates) {
 
-        // Initalize the query builder variables
+        // Initialize the query builder variables
         $qb = $this->_em->createQueryBuilder();
 
         // The query to get all page items
         $qb->select('DISTINCT p')
-                ->from('PageBundle:Page', 'p')
-                ->where(
-                        $qb->expr()->in('p.publishState', ':publishState')
+            ->from('PageBundle:Page', 'p')
+            ->where(
+                $qb->expr()->andX(
+                    $qb->expr()->in('p.publishState', ':publishState'),
+                    $qb->expr()->neq('p.pagetype', ':systemPageType')
                 )
-                ->orderBy('p.id', 'ASC')
-                ->setParameter('publishState', $publishStates)
+            )
+            ->orderBy('p.id', 'ASC')
+            ->setParameter('publishState', $publishStates)
+            ->setParameter('systemPageType', "system_page")
         ;
 
         // Get the results
@@ -220,24 +266,24 @@ class PageRepository extends EntityRepository {
     }
 
     // Function to define what page items will be returned for each paginated listing page
-    public function getPaginatedResults($qb, $totalResultsCount, $currentpage, $totalpageitems) {
+    public function getPaginatedResults($qb, $totalResultsCount, $currentPage, $totalPageItems) {
 
         $pages = null;
         $totalPages = 1;
 
         // Calculate and set the starting and last page item to retrieve
-        if ((isset($currentpage)) && (isset($totalpageitems))) {
-            if ($totalpageitems > 0) {
-                $startingItem = (intval($currentpage) * $totalpageitems);
+        if ((isset($currentPage)) && (isset($totalPageItems))) {
+            if ($totalPageItems > 0) {
+                $startingItem = (intval($currentPage) * $totalPageItems);
                 $qb->setFirstResult($startingItem);
-                $qb->setMaxResults($totalpageitems);
+                $qb->setMaxResults($totalPageItems);
             }
         }
 
         // Get paginated results
         $pages = $qb->getQuery()->getResult();
         // Get the total pagination pages
-        $totalPages = ceil($totalResultsCount / $totalpageitems);
+        $totalPages = ceil($totalResultsCount / $totalPageItems);
         // Set the page items and pagination to be returned
         $pageList = array('pages' => $pages, 'totalPages' => $totalPages);
 
